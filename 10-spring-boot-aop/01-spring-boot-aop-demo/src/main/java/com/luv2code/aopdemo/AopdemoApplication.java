fdc088e978c67475bb271aca0ef241aa733c8a92
Albert Robrigado <albertrobrigado@gmail.com>
2025-09-23 18:42:51 +0800

    add additional files in our spring aop tutorials

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -1,13 +1,39 @@
 package com.luv2code.aopdemo;
 
+import com.luv2code.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO accountDAO) {
+
+		return runner -> {
+
+			demoTheBeforeAdvice(accountDAO);
+
+		};
+	}
+
+	public void demoTheBeforeAdvice(AccountDAO accountDAO) {
+
+		// call the business method
+		accountDAO.addAccount();
+
+		// do it again!
+		System.out.println("\n let's call it again!\n");
+
+		// call the business method again
+		accountDAO.addAccount();
+
 	}
 
 }
