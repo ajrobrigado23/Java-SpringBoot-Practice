1345c7549bff0b638fe5eea2471d9f7f61244b90
Albert Robrigado <albertrobrigado.ict@gmail.com>
2025-09-28 10:08:21 +0800

    add new file

---
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -0,0 +1,39 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO accountDAO, MembershipDAO membershipDAO) {
+
+		return runner -> {
+
+			demoTheBeforeAdvice(accountDAO, membershipDAO);
+
+		};
+	}
+
+	public void demoTheBeforeAdvice(AccountDAO accountDAO, MembershipDAO membershipDAO) {
+
+		// call the business method
+		accountDAO.addAccount(new Account(), true);
+		accountDAO.doWork();
+
+		// call the membership business method
+		membershipDAO.addAccount();
+		membershipDAO.goToSleep();
+
+	}
+
+}
